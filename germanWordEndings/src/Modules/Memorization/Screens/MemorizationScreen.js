d6bd39c8e3a036954738572adba2a930e6bdc031
Uchi91 <uygarucar2@gmail.com>
2021-05-27 14:37:24 +0300

    Memorization Screen created

--- germanWordEndings/src/Modules/Memorization/Screens/MemorizationScreen.js
+++ germanWordEndings/src/Modules/Memorization/Screens/MemorizationScreen.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import {Text, View} from 'react-native'
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View, FlatList } from 'react-native'
 
+import { DataTable } from 'react-native-paper'
+import Metrics from '../../../StylingConstants/Metrics';
+import VisibilityToggle from '../Component/VisibilityToggle'
+import styles from '../Styles/MemorizationStyles'
+
+const DummyData = [
+    {
+        id: 1,
+        title: 'Only adjective',
+        content: 'kleiner Tisch'
+    },
+    {
+        id: 2,
+        title: 'only definite',
+        content: 'der Käse'
+    }
+]
 const Memorization = () => {
 
-    return(
+    const _renderItem = ({ item }) => {
+
+        return (
+            <>
+                <View style={styles.itemContainer}>
+                    <VisibilityToggle title={item.title} content={item.content} itemId={item.id} />
+                </View>
+            </>
+        )
+    }
+
+
+
+    return (
         <View>
-            <Text>You are in Memorization screen</Text>
+            <FlatList
+                data={DummyData}
+                renderItem={_renderItem}
+                keyExtractor={(item) => item.id}
+            />
         </View>
     )
 }
