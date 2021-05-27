0aadd7e5c08e360781494c53849f86c220761099
Uchi91 <uygarucar2@gmail.com>
2021-05-27 14:35:57 +0300

    Change in sentences (need a change into German )

--- germanWordEndings/src/Modules/Examples/SampleSentencesData.js
+++ germanWordEndings/src/Modules/Examples/SampleSentencesData.js
@@ -4,14 +4,14 @@ const SampleSentenceEnglish = {
     'accusative': {
         'masculine': {
             'with': {//there is adj.
-                'no': <Text>"I've seen good man"</Text>,//No article
-                'indefinite': <Text>"I've seen a good man"</Text>, //indefinite article
-                'definite': <Text>"I've seen the good man"</Text>//definite article
+                'no': <Text>I've seen good man</Text>,//No article
+                'indefinite': <Text>I've seen a good man</Text>, //indefinite article
+                'definite': <Text>I've seen the good man</Text>//definite article
             },
             'without': {//no adj.
-                'no': <Text>"I've seen man"</Text>, // No article
-                'indefinite': <Text>"I've seen a man"</Text>, //indefinite article
-                'definite': <Text>"I've seen the man"</Text> //definite article
+                'no': <Text>There is not a word with ending for this combination</Text>, // No article
+                'indefinite': <Text>I've seen a man</Text>, //indefinite article
+                'definite': <Text>I've seen the man</Text> //definite article
             }
         },
         'feminine': {
@@ -21,7 +21,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>I've seen the good woman</Text>
             },
             'without': {
-                'no': <Text>I've seen woman</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've seen a woman</Text>,
                 'definite': <Text>I've seen the woman</Text>
             }
@@ -33,7 +33,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>I've seen the good child</Text>
             },
             'without': {
-                'no': <Text>I've seen child</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've seen a child</Text>,
                 'definite': <Text>I've seen the child</Text>
             }
@@ -47,7 +47,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>I've helped to the good man</Text>
             },
             'without': {
-                'no': <Text>I've helped to man</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a man</Text>,
                 'definite': <Text>I've helped to the man</Text>
             }
@@ -59,7 +59,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>I've helped to the good woman</Text>
             },
             'without': {
-                'no': <Text>I've helped to woman</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a woman</Text>,
                 'definite': <Text>I've helped to the woman</Text>
             }
@@ -71,7 +71,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>I've helped to the good child</Text>
             },
             'without': {
-                'no': <Text>I've helped to child</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a child</Text>,
                 'definite': <Text>I've helped to the child</Text>
             }
@@ -85,7 +85,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>The good man was looking for you</Text>
             },
             'without': {
-                'no': <Text> Man was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A man was looking for you</Text>,
                 'definite': <Text>The man was looking for you</Text>
             }
@@ -97,7 +97,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>The good woman was looking for you</Text>
             },
             'without': {
-                'no': <Text>Woman was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A woman was looking for you</Text>,
                 'definite': <Text>The woman was looking for you</Text>
             }
@@ -109,7 +109,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>The good child was looking for you</Text>
             },
             'without': {
-                'no': <Text>Child was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A child was looking for you</Text>,
                 'definite': <Text>The child was looking for you</Text>
             }
@@ -123,7 +123,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>Money of the good man is missing</Text>
             },
             'without': {
-                'no': <Text>Money of man is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a man is missing</Text>,
                 'definite': <Text>Money of the man is missing</Text>
             }
@@ -135,7 +135,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>Money of the good woman is missing</Text>
             },
             'without': {
-                'no': <Text>Money of woman is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a woman is missing</Text>,
                 'definite': <Text>Money of the woman is missing</Text>
             }
@@ -147,7 +147,7 @@ const SampleSentenceEnglish = {
                 'definite': <Text>Money of the good child is missing</Text>
             },
             'without': {
-                'no': <Text>Money of child is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a child is missing</Text>,
                 'definite': <Text>Money of the child is missing</Text>
             }
@@ -171,36 +171,36 @@ const SampleSentenceGerman = {
     'accusative': {
         'masculine': {
             'with': {//there is adj.
-                'no': <Text>"Ich habe guten Mann gesehen"</Text>,//No article
-                'indefinite': <Text>"Ich habe einen guten Mann gesehen"</Text>,//indefinite article
-                'definite': <Text>"Ich habe den guten Mann gesehen"</Text>//definite article
+                'no': <Text>Ich habe guten Mann gesehen</Text>,//No article
+                'indefinite': <Text>Ich habe einen guten Mann gesehen</Text>,//indefinite article
+                'definite': <Text>Ich habe den guten Mann gesehen</Text>//definite article
             },
             'without': {//no adj.
-                'no': <Text>"I've seen man"</Text>, // No article
-                'indefinite': <Text>"I've seen a man"</Text>, //indefinite article
-                'definite': <Text>"I've seen the man"</Text> //definite article
+                'no': <Text>There is not a word with ending for this combination</Text>, // No article
+                'indefinite': <Text>I've seen a man</Text>, //indefinite article
+                'definite': <Text>I've seen the man</Text> //definite article
             }
         },
         'feminine': {
             'with': {
-                'no': <Text>I've seen good woman</Text>,
+                'no': <Text>Ich habe gute Frau gesehen</Text>,
                 'indefinite': <Text>I've seen a good woman</Text>,
                 'definite': <Text>I've seen the good woman</Text>
             },
             'without': {
-                'no': <Text>I've seen woman</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've seen a woman</Text>,
                 'definite': <Text>I've seen the woman</Text>
             }
         },
         'neutral': {
             'with': {
-                'no': <Text>I've seen good child</Text>,
+                'no': <Text>Ich habe gutes Kind gesehen</Text>,
                 'indefinite': <Text>I've seen a good child</Text>,
                 'definite': <Text>I've seen the good child</Text>
             },
             'without': {
-                'no': <Text>I've seen child</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've seen a child</Text>,
                 'definite': <Text>I've seen the child</Text>
             }
@@ -209,36 +209,36 @@ const SampleSentenceGerman = {
     'dative': {
         'masculine': {
             'with': {
-                'no': <Text>I've helped to good man</Text>,
+                'no': <Text>Ich habe gutem Mann geholfen</Text>,
                 'indefinite': <Text>I've helped to a good man</Text>,
                 'definite': <Text>I've helped to the good man</Text>
             },
             'without': {
-                'no': <Text>I've helped to man</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a man</Text>,
                 'definite': <Text>I've helped to the man</Text>
             }
         },
         'feminine': {
             'with': {
-                'no': <Text>I've helped to good woman</Text>,
+                'no': <Text>Ich habe guter Frau geholfen</Text>,
                 'indefinite': <Text>I've helped to a good woman</Text>,
                 'definite': <Text>I've helped to the good woman</Text>
             },
             'without': {
-                'no': <Text>I've helped to woman</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a woman</Text>,
                 'definite': <Text>I've helped to the woman</Text>
             }
         },
         'neutral': {
             'with': {
-                'no': <Text>I've helped to good child</Text>,
+                'no': <Text>Ich habe gutem Kind geholfen</Text>,
                 'indefinite': <Text>I've helped to a good child</Text>,
                 'definite': <Text>I've helped to the good child</Text>
             },
             'without': {
-                'no': <Text>I've helped to child</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>I've helped to a child</Text>,
                 'definite': <Text>I've helped to the child</Text>
             }
@@ -252,7 +252,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>The good man was looking for you</Text>
             },
             'without': {
-                'no': <Text> Man was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A man was looking for you</Text>,
                 'definite': <Text>The man was looking for you</Text>
             }
@@ -264,7 +264,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>The good woman was looking for you</Text>
             },
             'without': {
-                'no': <Text>Woman was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A woman was looking for you</Text>,
                 'definite': <Text>The woman was looking for you</Text>
             }
@@ -276,7 +276,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>The good child was looking for you</Text>
             },
             'without': {
-                'no': <Text>Child was looking for you</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>A child was looking for you</Text>,
                 'definite': <Text>The child was looking for you</Text>
             }
@@ -290,7 +290,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>Money of the good man is missing</Text>
             },
             'without': {
-                'no': <Text>Money of man is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a man is missing</Text>,
                 'definite': <Text>Money of the man is missing</Text>
             }
@@ -302,7 +302,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>Money of the good woman is missing</Text>
             },
             'without': {
-                'no': <Text>Money of woman is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a woman is missing</Text>,
                 'definite': <Text>Money of the woman is missing</Text>
             }
@@ -314,7 +314,7 @@ const SampleSentenceGerman = {
                 'definite': <Text>Money of the good child is missing</Text>
             },
             'without': {
-                'no': <Text>Money of child is missing</Text>,
+                'no': <Text>There is not a word with ending for this combination</Text>,
                 'indefinite': <Text>Money of a child is missing</Text>,
                 'definite': <Text>Money of the child is missing</Text>
             }
