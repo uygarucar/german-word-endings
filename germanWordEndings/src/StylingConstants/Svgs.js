95fc772bbfccd895f4ab8ff6613e8a5993219267
Uchi91 <uygarucar2@gmail.com>
2021-05-28 12:28:04 +0300

    Redundant Svg deleted

--- germanWordEndings/src/StylingConstants/Svgs.js
+++ germanWordEndings/src/StylingConstants/Svgs.js
@@ -1,9 +1,7 @@
 import ArrowDown from '../Assets/Svgs/arrow-down.svg'
 import ArrowRight from '../Assets/Svgs/arrow-right.svg'
-import OkayButton from '../Assets/Svgs/okay-button.svg'
 
 export default Svgs = {
     ArrowDown,
     ArrowRight,
-    OkayButton,
 }
