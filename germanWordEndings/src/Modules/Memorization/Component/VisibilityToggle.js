948cfdd90d4ea72b74f19f3c5092e124fc6322d9
Uchi91 <uygarucar2@gmail.com>
2021-05-28 12:29:11 +0300

    Styling applied from js file

--- germanWordEndings/src/Modules/Memorization/Component/VisibilityToggle.js
+++ germanWordEndings/src/Modules/Memorization/Component/VisibilityToggle.js
@@ -3,9 +3,10 @@ import { TouchableOpacity, View, Text } from 'react-native'
 import Svgs from '../../../StylingConstants/Svgs'
 import Metrics from '../../../StylingConstants/Metrics'
 import styles from '../Styles/VisibilityToggleStyle'
+import { DataTable } from 'react-native-paper'
 
 const VisibilityToggle = (props) => {
-    
+
     const [shouldShow, setShouldShow] = useState(false);
     const SvgRight = Svgs.ArrowRight;
     const SvgDown = Svgs.ArrowDown;
@@ -28,20 +29,100 @@ const VisibilityToggle = (props) => {
                     </TouchableOpacity>
                     :
                     <TouchableOpacity style={styles.iconWhenClicked}
-                    onPress={_changeVisibility}>
+                        onPress={_changeVisibility}>
                         <SvgDown width="40%" height="80%" />
                     </TouchableOpacity>
                 }
-                <TouchableOpacity  onPress={_changeVisibility}>
+                <TouchableOpacity onPress={_changeVisibility}>
                     <Text style={{ fontSize: 20 }}>{props.title}</Text>
                 </TouchableOpacity>
             </View>
-            <View>
+            {/* <View>
                 {
                     shouldShow ?
                         <Text>{props.content}</Text>
                         :
                         null}
+            </View> */}
+            <View>
+                {shouldShow ?
+                    <DataTable>
+                        <DataTable.Header style={styles.headerContainer}> 
+                            {/* <DataTable.Title>Only Adj.</DataTable.Title> */}
+                            <View style={styles.headerTitlesContainer}>
+                                <View><Text>Masculine</Text></View>
+                                <View><Text>Feminine</Text></View>
+                                <View><Text>Neutral</Text></View>
+                                <View><Text>Plural</Text></View>
+                            </View>
+                        </DataTable.Header>
+
+                        <DataTable.Row>
+                            <View style={styles.rowContainer}>
+                                <View style={styles.caseContainer} ><Text>Nominative</Text></View>
+                                <View style={styles.rowValuesContainer}>
+                                    <View style={styles.rowValuesInnerContainer}>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                    </View>
+                                </View>
+                            </View>
+                        </DataTable.Row>
+
+                        <DataTable.Row>
+                            <View style={styles.rowContainer}>
+                                <View style={styles.caseContainer} ><Text>Accusative</Text></View>
+                                <View style={styles.rowValuesContainer}>
+                                    <View style={styles.rowValuesInnerContainer}>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                    </View>
+                                </View>
+                            </View>
+                        </DataTable.Row>
+
+                        <DataTable.Row>
+                            <View style={styles.rowContainer}>
+                                <View style={styles.caseContainer} ><Text>Dative</Text></View>
+                                <View style={styles.rowValuesContainer}>
+                                    <View style={styles.rowValuesInnerContainer}>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                    </View>
+                                </View>
+                            </View>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <View style={styles.rowContainer}>
+                                <View style={styles.caseContainer} ><Text>Genitive</Text></View>
+                                <View style={styles.rowValuesContainer}>
+                                    <View style={styles.rowValuesInnerContainer}>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                        <View style={styles.rowItem}><Text>einer</Text><Text>gutem</Text></View>
+                                    </View>
+                                </View>
+                            </View>
+                        </DataTable.Row>
+                        {/* <DataTable.Pagination
+                            page={0}
+                            numberOfPages={2}
+                            onPageChange={page => {
+                                console.log(page);
+                            }}
+                            label="1-2 of 6"
+                        /> */}
+                    </DataTable>
+                    :
+                    null
+                }
             </View>
         </>
     )
--- germanWordEndings/src/Modules/Memorization/Styles/VisibilityToggleStyle.js
+++ germanWordEndings/src/Modules/Memorization/Styles/VisibilityToggleStyle.js
@@ -16,6 +16,39 @@ const styles = StyleSheet.create({
     iconWhenNotClicked:{
         paddingLeft: Metrics.width * 0.08,
         width: Metrics.width * 0.2
+    },
+    headerContainer:{
+        flexDirection:'row',
+        justifyContent: 'flex-end',
+        paddingLeft: Metrics.width * 0.01,
+    },
+    headerTitlesContainer: {
+        flexDirection: 'row',
+        width: Metrics.width * 0.76,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    rowContainer:{
+        flexDirection: 'row',
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    caseContainer:{
+        maxWidth: Metrics.width * 0.4,
+        alignSelf: 'center'
+    },
+    rowValuesContainer:{
+        width: Metrics.width * 0.7,
+    },
+    rowValuesInnerContainer:{
+        //
+        flexDirection:'row',
+        justifyContent:'space-between',
+        flex: 1
+    },
+    rowItem:{
+        paddingVertical: Metrics.height * 0.02,
+        alignSelf: 'center'
     }
 })
 
